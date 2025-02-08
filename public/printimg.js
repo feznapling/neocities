0d21214b2cf9a8584067e7ab43d032d0288dc395
Seb Fez <dahakasmurf@gmail.com>
2025-02-08 11:41:16 +0800

    api tests

--- public/printimg.js
+++ public/printimg.js
@@ -1,4 +1,24 @@
 
+// window.onload = function() {
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             var site_data = JSON.parse(this.responseText);
+//             let hits = site_data.info.views;
+        
+//             hits = String(hits).padStart(7, '0');
+        
+//             var hitsArr = hits.split("").map(Number);
+        
+//             for (let i = 0; i < hitsArr.length; i++) {
+//                 document.getElementById("num"+(7-i)).src = `./rsc/img/num${hitsArr[i]}.gif`;
+//             }
+//         }
+//     };
+//     xhttp.open("GET", "https://weirdscifi.ratiosemper.com/neocities.php?sitename=sebfez", true);
+//     xhttp.send();
+// }
+
 window.onload = function() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -15,6 +35,6 @@ window.onload = function() {
             }
         }
     };
-    xhttp.open("GET", "https://weirdscifi.ratiosemper.com/neocities.php?sitename=sebfez", true);
+    xhttp.open("GET", "https://neocities.org/api/info?sitename=sebfez", true);
     xhttp.send();
 }
