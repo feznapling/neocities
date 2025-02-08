b29c0286fa1ad2fb0fa0dc5ea0ab96af29a19592
Seb Fez <dahakasmurf@gmail.com>
2025-02-08 10:33:42 +0800

    website deploy

---
+++ public/printimg.js
@@ -0,0 +1,34 @@
+
+window.onload = function() {
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var site_data = JSON.parse(this.responseText);
+            let hits = site_data.info.views;
+        
+            hits = String(hits).padStart(7, '0');
+        
+            var hitsArr = hits.split("").map(Number);
+        
+            for (let i = 0; i < hitsArr.length; i++) {
+                document.getElementById("num"+(7-i)).src = `/rsc/num${hitsArr[i]}.gif`;
+            }
+        }
+    };
+    xhttp.open("GET", "https://weirdscifi.ratiosemper.com/neocities.php?sitename=sebfez", true);
+    xhttp.send();
+    /*
+    let site_data = getSiteData();
+    let hits = site_data.info.views;
+    
+    hits = String(hits).padStart(7, '0');
+    console.log(hits);
+    
+    var hitsArr = hits.split("").map(Number);
+    
+    for (let i = 0; i < hitsArr.length; i++) {
+        document.getElementById("num"+(7-i)).src = `/rsc/num${hitsArr[i]}.gif`;
+    } 
+    */
+}
